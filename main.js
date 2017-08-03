444414272cf26a8d2df1efec5571dcb3d2570735
Ronaldo Hornburg <ronaldo.hornburg@embraco.com>
2017-08-03 09:46:27 -0300

    On branch master  Changes to be committed: 	modified:   bin/www 	modified:   main.js 	new file:   npm-debug.log 	modified:   package.json 	modified:   routes/index.js 	new file:   routes/login.js 	modified:   routes/users.js 	renamed:    views/inicial.ejs -> views/index.ejs 	deleted:    views/index.original.ejs

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('dev'));  // combined, common, dev, short, tiny
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var pjson = require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('inicial', { title: 'PEN',
+  res.render('index', { title: 'PEN',
 								static_path: '',
 								theme: process.env.THEME || 'cerulean',
-								flask_debug: process.env.FLASK_DEBUG || 'false'
+								flask_debug: process.env.FLASK_DEBUG || 'false',
+                version: pjson.version
 							});
 });
 
